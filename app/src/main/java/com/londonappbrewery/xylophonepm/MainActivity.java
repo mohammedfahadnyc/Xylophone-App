94ee80c6f0f6ca737ae2d0729093fe4210577934
mohammedfahadnyc <fahadmohammed29299@gmail.com>
2021-10-25 03:21:27 -0400

    Initial commit

---
+++ app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
@@ -0,0 +1,88 @@
+package com.londonappbrewery.xylophonepm;
+
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Helpful Constants
+    private final int NR_OF_SIMULTANEOUS_SOUNDS = 7;
+    private final float LEFT_VOLUME = 1.0f;
+    private final float RIGHT_VOLUME = 1.0f;
+    private final int NO_LOOP = 0;
+    private final int PRIORITY = 0;
+    private final float NORMAL_PLAY_RATE = 1.0f;
+
+    // TODO: Add member variables here
+    private SoundPool mSoundPool;
+    private int mCSoundId;
+    private int mDSoundId;
+    private int mESoundId;
+    private int mFSoundId;
+    private int mGSoundId;
+    private int mASoundId;
+    private int mBSoundId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // TODO: Create a new SoundPool
+        mSoundPool = new SoundPool(7,AudioManager.STREAM_MUSIC,0);
+
+        // TODO: Load and get the IDs to identify the sounds
+        mCSoundId = mSoundPool.load(getApplicationContext(),R.raw.note1_c,1);
+        mDSoundId = mSoundPool.load(getApplicationContext(),R.raw.note2_d,1);
+        mESoundId = mSoundPool.load(getApplicationContext(),R.raw.note3_e,1);
+        mFSoundId = mSoundPool.load(getApplicationContext(),R.raw.note4_f,1);
+        mGSoundId = mSoundPool.load(getApplicationContext(),R.raw.note4_f,1);
+        mASoundId = mSoundPool.load(getApplicationContext(),R.raw.note6_a,1);
+        mBSoundId = mSoundPool.load(getApplicationContext(),R.raw.note7_b,1);
+
+    }
+    public void playC (View v)
+    {
+        Log.d("C","Played");
+        mSoundPool.play(mCSoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
+    }
+    public void playD (View v)
+    {
+        Log.d("D","Played");
+        mSoundPool.play(mDSoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
+    }
+    public void playE (View v)
+    {
+        Log.d("E","Played");
+        mSoundPool.play(mESoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
+    }
+    public void playF (View v)
+    {
+        Log.d("F","Played");
+        mSoundPool.play(mFSoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
+    }
+    public void playG (View v)
+    {
+        Log.d("G","Played");
+        mSoundPool.play(mGSoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
+    }
+    public void playA (View v)
+    {
+        Log.d("A","Played");
+        mSoundPool.play(mASoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
+    }
+    public void playB (View v)
+    {
+        Log.d("B","Played");
+        mSoundPool.play(mBSoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
+    }
+
+    // TODO: Add the play methods triggered by the buttons
+
+
+
+}
